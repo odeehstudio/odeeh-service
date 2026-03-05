85ecbcf7227803654bd631e72af4fc19c11f9c8b
Gilles Schelback <odeeh.studio@gmail.com>
2026-03-05 18:43:49 +0100

    Initial project

---
+++ src/main/java/be/odeeh/studio/odeehservice/OdeehServiceApplication.java
@@ -0,0 +1,13 @@
+package be.odeeh.studio.odeehservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdeehServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdeehServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/odeeh/studio/odeehservice/OdeehServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.odeeh.studio.odeehservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdeehServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
